bbe3f648fe81742eda02c06ff0698958c2483b5b
Elvis <elvisgiltorres@gmail.com>
2022-02-14 22:15:02 -0500

    Trabajando el formulario con setState y capturando sus valores

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,10 +3,15 @@ import '../components/styles/BadgeForm.css';
 
 
 class BadgeForm extends React.Component {
+    state={};
     handleChange = (e) =>{
-        console.log({
-            name: e.target.name,
-            value: e.target.value});
+        // console.log({
+        //     name: e.target.name,
+        //     value: e.target.value});
+
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
     }
     handleClick = (e) =>{
         console.log("Button was clicked");
@@ -15,6 +20,7 @@ class BadgeForm extends React.Component {
     handleSubmit = (e) =>{
         e.preventDefault();
         console.log("Form was Submitted");
+        console.log(this.state);
     }
 
 
@@ -24,19 +30,19 @@ class BadgeForm extends React.Component {
                     <h1>New User</h1>
                     <label className="form-firstName">
                         <p>First Name</p>
-                        <input onChange={this.handleChange} type="text" name="FirstName"/>
+                        <input onChange={this.handleChange} type="text" name="FirstName" value={this.state.FirstName}/>
                     </label> 
                     <label className="form-LastName">
                         <p>Last Name</p>
-                        <input type="text" name="LastName"/>
+                        <input onChange={this.handleChange} type="text" name="LastName" value={this.state.LastName}/>
                     </label> 
                     <label className="form-Email">
                         <p>Email</p>
-                        <input type="email" name="LastName"/>
+                        <input onChange={this.handleChange} type="email" name="Email" autoComplete="email" value={this.state.Email}/>
                     </label>
                     <label className="form-check">
-                        <input type="checkbox" />
-                        <span className="form-check-span">He leido las <span className="hipervinculos"><a href="/">Reglas de usuario</a></span> y la <span className="hipervinculos"><a href="/">Politica de seguridad</a></span></span>
+                        <input onChange={this.handleChange} type="checkbox" name="Checkbox" required/>
+                        <span className="form-check-span">He leido las <span><a href="/">Reglas de usuario</a></span> y la <span><a href="/">Politica de seguridad</a></span></span>
                     </label>
                     <button onClick={this.handleClick} className="form-button">Save</button> 
                 </form>
